3d602cd13e867dca732967b5fd5d91c7376cb4c7
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-01 21:20:11 +0530

    feat: add UserMenu component with dropdown functionality

---
+++ client/src/components/UserMenu.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "wouter";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { User, Crown, Settings, HelpCircle, LogOut } from "lucide-react";
+
+export default function UserMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="relative h-8 w-8 rounded-full bg-transparent focus:outline-none">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/avatars/01.png" alt="User" />
+            <AvatarFallback>VU</AvatarFallback>
+          </Avatar>
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Vibe User</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              user@vibes.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/profile">
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link href="/premium">
+            <Crown className="mr-2 h-4 w-4" />
+            <span>Upgrade to Premium</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Settings className="mr-2 h-4 w-4" />
+          <span>Settings</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <HelpCircle className="mr-2 h-4 w-4" />
+          <span>Help & Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
