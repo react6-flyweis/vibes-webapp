31f08b0780c5a4abf4e5a04034e073c3cd14cef5
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-04 00:22:43 +0530

    feat: enhance Topbar with authentication handling and conditional rendering for login/signup/logout

--- client/src/components/Topbar/TopBar.tsx
+++ client/src/components/Topbar/TopBar.tsx
@@ -1,19 +1,43 @@
 import { Button } from "../ui/button";
 import UserMenu from "../UserMenu";
 import LogoHeart from "./LogoHeart";
+import { Link } from "wouter";
+import { useAuthStore } from "@/store/auth-store";
+import { LogOutIcon } from "lucide-react";
 
 const Topbar: React.FC = () => {
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated());
+  const logout = useAuthStore((s) => s.logout);
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <nav className="w-full h-12 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700shadow p-3">
+    <nav className="w-full h-12 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 shadow p-3">
       <div className="flex items-center justify-between max-w-7xl mx-auto h-full">
         <LogoHeart />
 
-        <div className="flex gap-5">
-          <Button variant="outline">Login</Button>
-          <Button className="bg-gradient-cta">Sign Up</Button>
+        <div className="flex gap-5 items-center">
+          {!isAuthenticated ? (
+            <>
+              <Button asChild variant="outline">
+                <Link href="/login">Login</Link>
+              </Button>
+              <Button asChild className="bg-gradient-cta">
+                <Link href="/signup">Sign Up</Link>
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button variant="ghost" onClick={handleLogout}>
+                <LogOutIcon />
+                Logout
+              </Button>
+              <UserMenu />
+            </>
+          )}
         </div>
-
-        {/* <UserMenu /> */}
       </div>
     </nav>
   );
