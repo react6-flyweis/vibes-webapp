2af98b6f7d028f9b03ec577185bbb775541aedea
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-03 23:42:12 +0530

    fix: standardize import statements and add SVG module declaration

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,15 @@
 // In src/global.d.ts or another .d.ts file
 
-import 'react';
+import "react";
 
-declare module 'react' {
+declare module "react" {
   interface CSSProperties {
     [key: `--${string}`]: string | number;
   }
+}
+
+// Allow importing SVGs as URL strings (used with <img src={...} />)
+declare module "*.svg" {
+  const src: string;
+  export default src;
 }
