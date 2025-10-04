e8a80e083e9aa345dc1368d8000779081189fed3
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-04 23:28:06 +0530

    fix: adjust column classes in AuthLayout for consistent layout on medium screens

--- client/src/components/auth/AuthLayout.tsx
+++ client/src/components/auth/AuthLayout.tsx
@@ -4,10 +4,10 @@ import vendorAuthImage from "@/assets/images/vendor-auth.svg";
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen grid grid-cols-12 bg-gradient-primary">
-      <div className="col-span-12 lg:col-span-6 flex flex-col justify-center h-full overflow-y-auto  p-8 bg-white">
+      <div className="col-span-12 md:col-span-6 flex flex-col justify-center h-full overflow-y-auto  p-8 bg-white">
         {children}
       </div>
-      <div className="col-span-12 lg:col-span-6 flex flex-col justify-center items-center p-8">
+      <div className="col-span-12 md:col-span-6 flex flex-col justify-center items-center p-8">
         <img src={userAuthImage} className="" />
       </div>
     </div>
