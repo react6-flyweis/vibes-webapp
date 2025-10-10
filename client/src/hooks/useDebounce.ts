b9b1296f904a87a8de0d340b0e647b1c2eb84cb8
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-11 00:05:15 +0530

    feat: add useDebounce hook for debouncing values with a customizable delay

---
+++ client/src/hooks/useDebounce.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounce<T>(value: T, delay = 300) {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(id);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
