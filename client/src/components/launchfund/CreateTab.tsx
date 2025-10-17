dd7e3562466f7a124cef87c84bf8035b6431c520
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-17 23:24:32 +0530

    feat: add CreateTab component for launching new campaigns

---
+++ client/src/components/launchfund/CreateTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CampaignCreator from "@/components/campaign/CampaignCreator";
+
+const CreateTab: React.FC = () => {
+  return (
+    <div className="py-2">
+      <CampaignCreator />
+    </div>
+  );
+};
+
+export default CreateTab;
