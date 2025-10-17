964a45c21d2cc85ca63dfc3d5177716242de1f19
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-17 23:23:13 +0530

    feat: add launchfund index file to export campaign-related components

---
+++ client/src/components/launchfund/index.ts
@@ -0,0 +1,5 @@
+export { default as DiscoverTab } from "./DiscoverTab";
+export { default as FeaturedTab } from "./FeaturedTab";
+export { default as CreateTab } from "./CreateTab";
+export { default as MyCampaignsTab } from "./MyCampaignsTab";
+export { default as SmartAITab } from "./SmartAITab";
