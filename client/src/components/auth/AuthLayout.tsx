bb18c33d6e832bf4b42e2a9620ac9a136a33ec1f
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-10-24 02:03:26 +0530

    refactor: improve layout structure and styling in AuthLayout component

--- client/src/components/auth/AuthLayout.tsx
+++ client/src/components/auth/AuthLayout.tsx
@@ -1,14 +1,22 @@
 import userAuthImage from "@/assets/images/user-auth.svg";
-import vendorAuthImage from "@/assets/images/vendor-auth.svg";
+// import vendorAuthImage from "@/assets/images/vendor-auth.svg";
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen grid grid-cols-12 bg-gradient-primary">
-      <div className="col-span-12 md:col-span-6 flex flex-col justify-center h-full overflow-y-auto  p-8 bg-white">
+    <div className="min-h-screen grid grid-cols-12 bg-gradient-primary">
+      {/* Content column */}
+      <div className="col-span-12 md:col-span-6 flex flex-col justify-center h-full overflow-y-auto p-6 md:p-12 bg-white">
         {children}
       </div>
-      <div className="col-span-12 md:col-span-6 flex flex-col justify-center items-center p-8">
-        <img src={userAuthImage} className="" />
+
+      {/* Illustration column */}
+      <div className="col-span-12 md:col-span-6 flex flex-col justify-center items-center p-6 md:p-12">
+        <img
+          src={userAuthImage}
+          alt="Authentication illustration"
+          className="w-full max-w-[320px] sm:max-w-[420px] md:max-w-[520px] h-auto object-contain"
+          aria-hidden="false"
+        />
       </div>
     </div>
   );
