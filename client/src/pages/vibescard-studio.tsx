0758909f1d0a95244b7b54c2097d7f4e39f95569
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-11-04 16:23:23 +0530

    feat: pass design data to EditorPage in VibesCardStudioNew component

--- client/src/pages/vibescard-studio.tsx
+++ client/src/pages/vibescard-studio.tsx
@@ -1,11 +1,14 @@
-import Navigation from "@/components/navigation";
 import EditorPage from "@/features/vibescard-studio/EditorPage";
+import { useLocation } from "react-router";
 
 export default function VibesCardStudioNew() {
+  const location = useLocation();
+
+  const designData = location.state?.designData;
+
   return (
     <div className="min-h-screen bg-[#111827]">
-      <Navigation />
-      <EditorPage />
+      <EditorPage designData={designData} />
     </div>
   );
 }
