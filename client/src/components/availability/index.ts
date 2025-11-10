f85c7e41eab92a5a4af80ca635ad7fb72006de60
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-11-10 23:42:01 +0530

    feat: add Availability components for user/staff availability checks and booking display

---
+++ client/src/components/availability/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Availability Components
+ *
+ * Reusable components for checking and displaying user/staff availability
+ * All components accept a userId prop and automatically fetch bookings
+ */
+
+export {
+  AvailabilityTimeSlotSelector,
+  TIME_SLOTS,
+} from "../AvailabilityTimeSlotSelector";
+
+export { AvailabilityDateSelector } from "../AvailabilityDateSelector";
+
+export { AvailabilityDateRangeSelector } from "../AvailabilityDateRangeSelector";
+
+// Re-export hooks and utilities for convenience
+export {
+  useStaffAvailability,
+  isTimeSlotBooked,
+  isDateBooked,
+  isDateRangeBooked,
+  type AvailabilityBooking,
+} from "@/hooks/useStaffAvailability";
