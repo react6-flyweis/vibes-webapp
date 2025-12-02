c60893463ac6b0abe9ce27fc1477e03eb2336383
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-02 22:25:53 +0530

    fix: update prop name for default event type in CreateEvent component

--- client/src/pages/create-event.tsx
+++ client/src/pages/create-event.tsx
@@ -2,5 +2,5 @@ import { EventCreator } from "@/components/EventCreator";
 import React from "react";
 
 export default function CreateEvent() {
-  return <EventCreator eventType="private" />;
+  return <EventCreator defaultEventType="private" />;
 }
